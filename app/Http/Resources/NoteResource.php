c8bddd7145b8e32e8f4744294568e94b552abb9a
elforgaani <m.elforgaani@gmail.com>
2023-12-26 11:25:56 +0200

    Add resource.

---
+++ app/Http/Resources/NoteResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NoteResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'note_label' => $this->note_label,
+            'note_body' => $this->note_body,
+            'user_id' => $this->user_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
