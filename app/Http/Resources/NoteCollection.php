5e103d9de2408e0c302294d62d493e594b34d58d
elforgaani <m.elforgaani@gmail.com>
2023-12-26 11:26:30 +0200

    Add NoteCollection.

---
+++ app/Http/Resources/NoteCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class NoteCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'links' => [
+                'self' => '/api/notes'
+            ],
+            'meta' => [
+                'count' => $this->collection->count(),
+                'language' => 'english'
+            ],
+            'data' => $this->collection,
+        ];
+    }
+}
