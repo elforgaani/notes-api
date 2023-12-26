17ebb73ee78cc65dbe4396fda98ae70692ed15f4
elforgaani <m.elforgaani@gmail.com>
2023-12-26 09:01:00 +0200

    Add Seeder to Notes Model

--- database/seeders/NoteSeeder.php
+++ database/seeders/NoteSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Note;
 
 class NoteSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class NoteSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Note::factory()->count(10)->create();
     }
 }
